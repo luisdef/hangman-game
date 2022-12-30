4a499dc775ae3a044ec40c99931ae2b124378774
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2022-12-29 22:10:21 -0300

    Linter changes

--- assets/js/setLanguageByBrowser.js
+++ assets/js/setLanguageByBrowser.js
@@ -1,5 +1,5 @@
 // Set the language of the page by getting the browser information.
 
-const userLang = navigator.language || navigator.userLanguage; 
+const userLang = navigator.language || navigator.userLanguage;
 const htmlPageTag = document.querySelector("html");
 htmlPageTag.setAttribute("lang", userLang);
