bb4398adfb45255a305f73cf5917df813a229309
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2023-01-13 15:01:48 -0300

    Show the nickname of the user when the game starts

--- assets/js/verifyAndShowNickname.js
+++ assets/js/verifyAndShowNickname.js
@@ -2,7 +2,7 @@ const nickname = localStorage.getItem("nickname");
 const playersNameArea = document.getElementById("player");
 
 if (nickname) {
-    playersNameArea.innerHTML += nickname;
+    playersNameArea.innerHTML = nickname + " está jogando";
 } else {
     window.location.href = "index.html";
 }
