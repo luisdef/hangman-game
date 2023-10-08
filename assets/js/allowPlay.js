9759b1b9a2af4fc8c82365d6fd01a0699b9e9797
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2023-10-08 12:35:25 -0300

    Home page congiguration

--- assets/js/allowPlay.js
+++ assets/js/allowPlay.js
@@ -1,46 +1,46 @@
-const nickname = localStorage.getItem("nickname");
+const nickname = localStorage.getItem("nickname")
 
-const buttonResetDataFromPlayer = document.getElementById("reset-nickname");
-const tagPlayerName = document.getElementById("player-name");
-const tagHighScore = document.getElementById("highscore");
+const buttonResetDataFromPlayer = document.getElementById("reset-nickname")
+const tagPlayerName = document.getElementById("player-name")
+const tagHighScore = document.getElementById("highscore")
 
-const explanationWithoutUserData = document.querySelector(".no-data");
-const explanationWithUserData = document.querySelector(".with-data");
+const explanationWithoutUserData = document.querySelector(".no-data")
+const explanationWithUserData = document.querySelector(".with-data")
 
-const nicknameArea = document.getElementById("nickname");
-const playButton = document.getElementById("play-btn");
+const nicknameArea = document.getElementById("nickname")
+const playButton = document.getElementById("play-btn")
 
 if (nickname) {
-    explanationWithoutUserData.style.display = "none";
-    tagPlayerName.innerHTML += nickname;
-    playButton.style.display = "block";
-    nicknameArea.style.display = "none";
+  explanationWithoutUserData.style.display = "none"
+  tagPlayerName.innerHTML += nickname
+  playButton.style.display = "block"
+  nicknameArea.style.display = "none"
 
-    playButton.onclick = () => {
-        window.location.href = "game.html";
-    };
+  playButton.onclick = () => {
+    window.location.href = "game.html"
+  }
 } else {
-    explanationWithUserData.style.display = "none";
-    tagPlayerName.style.display = "none";
-    tagHighScore.style.display = "none";
-    buttonResetDataFromPlayer.style.display = "none";
+  explanationWithUserData.style.display = "none"
+  tagPlayerName.style.display = "none"
+  tagHighScore.style.display = "none"
+  buttonResetDataFromPlayer.style.display = "none"
 
-    function verifyIfHasANickname() {
-        if (nicknameArea.value.length >= 3 && nicknameArea.value.length <= 15) {
-            playButton.style.display = "block";
-        } else {
-            playButton.style.display = "none";
-        }
+  function verifyIfHasANickname() {
+    if (nicknameArea.value.length >= 3 && nicknameArea.value.length <= 15) {
+      playButton.style.display = "block"
+    } else {
+      playButton.style.display = "none"
     }
+  }
 
-    verifyIfHasANickname();
+  verifyIfHasANickname()
 
-    nicknameArea.oninput = () => {
-        verifyIfHasANickname();
-    };
+  nicknameArea.oninput = () => {
+    verifyIfHasANickname()
+  }
 
-    playButton.onclick = () => {
-        localStorage.setItem("nickname", nicknameArea.value);
-        window.location.href = "game.html";
-    };
+  playButton.onclick = () => {
+    localStorage.setItem("nickname", nicknameArea.value)
+    window.location.href = "game.html"
+  }
 }
