ec6a7af631fce6346b09bf9490fe856cfd2661a6
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2023-10-08 12:36:33 -0300

    Generate blank spaces of the word

--- assets/js/generateWordSpaces.js
+++ assets/js/generateWordSpaces.js
@@ -1,33 +1,32 @@
-const boxOfTheWord = document.querySelector("div.word");
-const boxOfHints = document.querySelector("div.hints");
+const boxOfTheWord = document.querySelector("div.word")
+const boxOfHints = document.querySelector("div.hints")
 
-export let lettersToBeDiscovered = [];
+export let lettersToBeDiscovered = []
 
 fetch("assets/js/contentOfWords.json")
-    .then((response) => {
-        return response.json();
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    const arrayOfWords = data.data
+    const raffledIndex = Math.floor(Math.random() * arrayOfWords.length)
+
+    const raffledWordObject = arrayOfWords.at(raffledIndex)
+
+    console.log(raffledWordObject)
+
+    let defaulHint = document.createElement("p")
+    defaulHint.innerText = raffledWordObject.hints[0]
+    boxOfHints.appendChild(defaulHint)
+
+    const arrayOfLetters = raffledWordObject.word.split("")
+    arrayOfLetters.forEach((letter) => {
+      let letterOfTheRaffledWord = document.createElement("span")
+      letterOfTheRaffledWord.className = "letter not-discovered"
+      letterOfTheRaffledWord.innerText = "?"
+
+      lettersToBeDiscovered.push(letter)
+
+      boxOfTheWord.appendChild(letterOfTheRaffledWord)
     })
-    .then((data) => {
-        const arrayOfWords = data.data;
-        const raffledIndex = Math.floor(Math.random() * arrayOfWords.length);
-
-        const raffledWordObject = arrayOfWords.at(raffledIndex);
-
-        console.log(raffledWordObject);
-
-        let defaulHint = document.createElement("p");
-        defaulHint.innerText = raffledWordObject.hints[0];
-        boxOfHints.appendChild(defaulHint);
-
-        const arrayOfLetters = raffledWordObject.word.split("");
-        arrayOfLetters.forEach((letter) => {
-            let letterOfTheRaffledWord = document.createElement("span");
-            letterOfTheRaffledWord.className = "letter not-discovered";
-            letterOfTheRaffledWord.innerText = "?";
-
-            lettersToBeDiscovered.push(letter);
-
-            boxOfTheWord.appendChild(letterOfTheRaffledWord);
-        });
-
-    });
+  })
